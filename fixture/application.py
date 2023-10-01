156332efbdb3a7f90c8e79034ddb485e257f82f6
artcrw <artemkropotin16@gmail.com>
2023-10-01 13:54:48 +0300

    Задание №25

---
+++ fixture/application.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from fixture.session import SessionHelper
+
+
+class Application:
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "safari":
+            self.wd = webdriver.Safari()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.wd.implicitly_wait(1)
+        self.session = SessionHelper(self)
+        self.base_url = base_url
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def open_home_page(self):
+        wd = self.wd
+        if not (len(wd.find_elements_by_name("add")) > 0 and len(wd.find_elements_by_name("to_group")) > 0):
+            wd.get(self.base_url)
+
+    def destroy(self):
+        self.wd.quit()
